7b0cbadb6bd4024d9568edf6b459740fa8f69589
Yassinmoh <yassin_mohamed310.com>
2024-06-11 11:20:25 +0300

    init

---
+++ src/app/app.config.ts
@@ -0,0 +1,10 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { reducers, metaReducers } from './store';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideStore(reducers, { metaReducers })]
+};
