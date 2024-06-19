5a5f1be345e18bfa08e7b37ea6cf72c5b43f132d
Yassinmoh <yassin_mohamed310.com>
2024-06-19 18:52:31 +0300

    Feat(Hoem): add header

--- src/app/modules/shared/components/header/header.component.ts
+++ src/app/modules/shared/components/header/header.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
 
 @Component({
   selector: 'Gosto-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  breakpointObserver:BreakpointObserver = inject(BreakpointObserver);
+  isMobile!:boolean;
+  ngOnInit(): void {
+    this.breakpointObserver.observe([Breakpoints.HandsetPortrait, Breakpoints.HandsetLandscape])
+      .subscribe(result => this.isMobile = result.matches);
+  }
 }
