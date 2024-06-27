9e175c2aa85e9d7b63caa1cdf31903d6685bc811
Yassinmoh <yassin_mohamed310.com>
2024-06-27 23:28:06 +0300

    Feat(Home): enhance top selling products

--- src/app/modules/home/components/top-selling-products/top-selling-products.component.ts
+++ src/app/modules/home/components/top-selling-products/top-selling-products.component.ts
@@ -2,13 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
 import { SellingProductCardComponent } from '../../../shared/components/cards/selling-product-card/selling-product-card.component';
 import { ProductService } from '../../../core/services/product.service';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { Product } from '../../../core/models/product';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'Gosto-top-selling-products',
   standalone: true,
-  imports: [CommonModule, SellingProductCardComponent],
+  imports: [CommonModule, SellingProductCardComponent,CarouselModule],
   templateUrl: './top-selling-products.component.html',
   styleUrl: './top-selling-products.component.scss'
 })
@@ -16,7 +17,39 @@ export class TopSellingProductsComponent implements OnInit {
 
   productService = inject(ProductService);
   products$!: Observable<Product[]>
+  products: Product[] = [];
+  tags: string[] = ['All Products','Smart','Watchs','Iphone','Games','Labtops'];
+  currentIndex = 0
+
+
+  customOptions: OwlOptions = {
+    loop: true,
+    autoplay: true,
+    center: false,
+    dots: false,
+    rtl: false,
+    autoHeight: true,
+    autoWidth: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1023: {
+        items: 4,
+      }
+    }
+  }
+
   ngOnInit(): void {
-    this.products$ = this.productService.getProducts()
+    this.products$ = this.productService.getProducts().pipe(
+      tap(data => this.products = data)
+    )
+  }
+
+  select(index:number){
+    this.currentIndex =index
   }
 }
--- src/app/modules/shared/components/cards/selling-product-card/selling-product-card.component.ts
+++ src/app/modules/shared/components/cards/selling-product-card/selling-product-card.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '../../../../core/models/product';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'Gosto-selling-product-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './selling-product-card.component.html',
   styleUrl: './selling-product-card.component.scss'
 })
 export class SellingProductCardComponent{
-
+  @Input() product!:Product
 }
