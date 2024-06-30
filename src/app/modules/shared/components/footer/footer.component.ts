f833cf36ee92cb51fc14e0f8aa6ed00dd59c9199
Yassinmoh <yassin_mohamed310.com>
2024-06-30 23:28:08 +0300

    Feat(Home): Add footer

--- src/app/modules/shared/components/footer/footer.component.ts
+++ src/app/modules/shared/components/footer/footer.component.ts
@@ -9,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
+  topFooterData=[
+    {
+      img:'FAST_DELIVERY.png',
+      title:'FAST DELIVERY',
+      description:'With sites in 6 languages, we ship to over 200 countries & regions.'
+    },
+    {
+      img:'SAFE_PAYMENT.png',
+      title:'SAFE PAYMENT',
+      description:'Money back up to 60 days, commitment to outstanding quality products.'
+    },
+    {
+      img:'DAYS_RETURN.png',
+      title:'60 DAYS RETURN',
+      description:'Round-the-clock assistance for a smooth shopping experience.'
+    },
+    {
+      img:'HELP_CENTER.png',
+      title:'HELP CENTER',
+      description:'Pay with the world’s most popular and secure payment methods.'
+    }
+  ]
 }
