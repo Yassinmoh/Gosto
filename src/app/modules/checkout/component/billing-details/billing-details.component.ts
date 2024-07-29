d53d3da2ea436ab8a5d8fc33d2ca6f006bef287e
ymohamed <ymohamed@sarmady.net>
2024-07-29 18:20:18 +0300

    Feat(Checkout): Add checkout page

---
+++ src/app/modules/checkout/component/billing-details/billing-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'Gosto-billing-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './billing-details.component.html',
+  styleUrl: './billing-details.component.scss'
+})
+export class BillingDetailsComponent {
+
+}
--- src/app/modules/checkout/pages/checkout-page/checkout-page.component.ts
+++ src/app/modules/checkout/pages/checkout-page/checkout-page.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AccordionModule } from 'primeng/accordion';
+import { BillingDetailsComponent } from '../../component/billing-details/billing-details.component';
+
 
 @Component({
   selector: 'Gosto-checkout-page',
   standalone: true,
-  imports: [],
+  imports: [AccordionModule, ReactiveFormsModule,BillingDetailsComponent],
   templateUrl: './checkout-page.component.html',
   styleUrl: './checkout-page.component.scss'
 })
-export class CheckoutPageComponent {
+export class CheckoutPageComponent implements OnInit {
 
+  _fb = inject(FormBuilder);
+
+  couponForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.initCouponForm()
+  }
+
+  initCouponForm() {
+    this.couponForm = this._fb.group({
+      coupon: ['']
+    })
+  }
+
+  applayCoupon(event:Event){
+    event.preventDefault()
+  }
 }
