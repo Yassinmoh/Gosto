cd53208173ba0c3fb6443677040fe00b7bf12b67
Yassinmoh <yassin_mohamed310.com>
2024-08-05 20:44:21 +0300

    Feat(Order): Update Order page

--- src/app/modules/orders/pages/orders-page/orders-page.component.ts
+++ src/app/modules/orders/pages/orders-page/orders-page.component.ts
@@ -3,11 +3,12 @@ import { Store } from '@ngrx/store';
 import { OrderState } from '../../../../store/Order/order.reducer';
 import { Observable, tap } from 'rxjs';
 import { getOrder } from '../../../../store/Order/order.selectors';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'Gosto-orders-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './orders-page.component.html',
   styleUrl: './orders-page.component.scss'
 })
@@ -15,14 +16,20 @@ export class OrdersPageComponent implements OnInit {
 
   _store = inject(Store<OrderState>)
   order$!: Observable<any>
-
+  date:any
   ngOnInit(): void {
     this.getOrderDetails()
+    this.getCurrentDate()
   }
 
   getOrderDetails(){
-    this._store.select(getOrder).pipe(
+    this.order$ =  this._store.select(getOrder).pipe(
       tap(data => console.log("Order Details",data))
-    ).subscribe()
+    )
+  }
+
+  getCurrentDate(){
+    this.date = new Date()
+    return this.date
   }
 }
